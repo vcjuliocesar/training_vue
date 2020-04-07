ed97e02e0f758f20cc5bd651d95b4e08be1303bb
julio cesar <vcjuliocesar@outlook.com>
2020-04-07 15:57:11 -0500

    Event Dispatcher

---
+++ Episode13-Component_Communication_Example#2:EventDispatcher/main.js
@@ -0,0 +1,24 @@
+window.Event = new Vue();
+
+Vue.component('coupon',{
+    template:`
+        <input placeholder="Enter your coupon code" @blur="onCouponApplied">
+    `,
+    methods:{
+        onCouponApplied(){
+            Event.$emit('applied');
+        }
+    }
+});
+
+
+new Vue({
+    el:"#root",
+
+    data:{
+        couponApplied:false
+    },
+    created() {
+        Event.$on('applied',()=>alert('Handling it'));
+    },
+})
