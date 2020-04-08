8b5e8e7159b1365391b50f97d2da1a96d4af9a5a
julio cesar <vcjuliocesar@outlook.com>
2020-04-08 13:49:30 -0500

    webpack and vuw cli

---
+++ Episode16-Webpack_and_vue-cli/my-app/src/componets/Message.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box is-info">
+       <p>
+           <slot></slot>
+       </p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="">
+    .box{
+        background: gray;
+        padding: 10px;
+        border: 1px solid green;
+    }
+</style>
